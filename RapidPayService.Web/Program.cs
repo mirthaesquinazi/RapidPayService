d05e705794d5b14d17330d22732777dfc3ba829e
Mir Esquinazi <mirtha.esquinazi@distillery.com>
2022-07-08 14:58:21 -0300

    Final commit.

--- RapidPayService.Web/Program.cs
+++ RapidPayService.Web/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using RapidPayService.Web;
 
-namespace RapidPayService
+namespace RapidPayService.Web
 {
     public class Program
     {
